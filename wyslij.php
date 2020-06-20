05b1a1403c0fd7cbcbd41dbac25a5bca43a9d23a
Mike <66726179+insolt@users.noreply.github.com>
2020-06-20 17:35:22 +0100

    Add files via upload

---
+++ wyslij.php
@@ -0,0 +1,25 @@
+﻿<?php
+if (!nick || !mail || !temat || !tresc)
+{    
+echo "Nie uzupełniłeś wszystkich rubryk.";
+exit;  
+}
+
+$wiadomosc =
+"Imie: ".$_POST['nick']."
+
+e- mail: ".$_POST['mail']."
+
+Tresc: ".$_POST['tresc']."  ";
+
+$tematyka = "www.labomed.pl - ".$temat;
+
+
+mail ("labomed@labomed.pl", $tematyka, $wiadomosc );
+echo "Dziekujemy za wysłanie do nas wiadomości,";
+echo "<ul>o treści: <ul />";
+echo "Twoje imię: <strong>".$nick."</strong><ul />";
+echo "Temat: <strong>".$temat."</strong><ul />";
+echo "Twój adres e-mail: <strong>".$mail."</strong><ul />";
+echo "Treść: <strong>".$tresc."</strong><ul />";
+?>
